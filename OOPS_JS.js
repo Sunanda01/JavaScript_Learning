a3935bfc1e5f8fe6844fc46b8be8843354cffcee
Sunanda01 <sunandasadhukhan1@gmail.com>
2024-01-15 21:04:08 +0530

    File Modified

--- OOPS_JS.js
+++ OOPS_JS.js
@@ -117,6 +117,23 @@
                             //OR
         const mycolor=[...color,'green','pink'];
         console.log(mycolor);           //[ 'red', 'blue', 'yellow', 'green', 'pink' ]
+//SHALLOW COPY & DEEP COPY
+    //SHALLOW COPY=changing the value of cloned object will reflect into original as well as because both are pointing to same reference object
+        const a={id:101, name:"Sunanda"};   //Original Object
+        const b=a;                          //Cloned Object
+        b.id=102;
+        console.log(a);         //{ id: 102, name: 'Sunanda' }
+        console.log(b);         //{ id: 102, name: 'Sunanda' }
+    //DEEP COPY=changing the value of object will not reflect into original object because both are pointing to different reference object
+        //Spread Operator
+        const c={...a};
+        c.id=103;
+        console.log(a);         //{ id: 102, name: 'Sunanda' }
+        console.log(c);         //{ id: 103, name: 'Sunanda' }
+        //Assign Operator
+        const d=Object.assign({},a);
+        d.id=104;
+        console.log(d);         //{ id: 104, name: 'Sunanda' }
 //ES7 FEATURES      2016
     //1. ARRAY INCLUDES
         const color2=['red','blue','yellow','green','pink'];
