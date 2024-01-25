c4c93b9c120e01e5c4b19ffe40c9cdd46ae18979
Sunanda01 <sunandasadhukhan1@gmail.com>
2024-01-25 18:31:50 +0530

    Update

--- OOPS/9StaticMethod.js
+++ OOPS/9StaticMethod.js
@@ -1,8 +1,10 @@
 //STATIC METHOD
 class Car {
+  static a=10;
     constructor(name) {
       this._name = name;
     }
+    
     static hello(x) {
       return "Hello " + x._name;
     }
@@ -10,3 +12,4 @@ class Car {
   const myCar = new Car("Ford");
   // console.log(myCar.hello(Ford));   //Error Message
   console.log(Car.hello(myCar));       //Hello Ford
+  console.log(Car.a); 
