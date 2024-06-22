13be54e26b0a3beb303dafd3d84a4e80041394a1
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2024-06-22 22:04:37 +0530

    Create 9.2StaticFunctionAndNormalFunction.js

---
+++ OOPS/9.2StaticFunctionAndNormalFunction.js
@@ -0,0 +1,18 @@
+class config{
+    static a1={
+        1:'hello'
+    };
+    //using this keyword for accessing a static property inside a static method
+    static show(){
+        console.log('static show');
+        console.log(this.a1);
+    }
+    //accessing a static function inside a normal fuction
+    display(){
+        console.log('display show 1');
+        config.show();
+    }
+}
+config.show(); //calling static fx
+const a=new config(); //calling normal function
+a.display();
