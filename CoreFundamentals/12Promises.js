d5179a6d955cf7b3b6ec6f1e5f7b31968d942c10
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2024-07-15 15:26:45 +0530

    Update 12Promises.js

--- CoreFundamentals/12Promises.js
+++ CoreFundamentals/12Promises.js
@@ -104,4 +104,4 @@ let p3=new Promise((resolve,reject)=>{
     setTimeout(()=>resolve("p3 success"),2000);
 })
 Promise.any([p1,p2,p3]).then((res)=>{console.log(res);})
-.catch((err)=>{console.error(err)});
+.catch((err)=>{console.log(err.errors)});
